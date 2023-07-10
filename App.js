ab38181cfc67ee3d0debf583109918ee425792a3
BEZYL MOPHAT OTIENO <96384227+Bezyl-Mophat-Otieno@users.noreply.github.com>
2023-07-10 18:04:47 +0300

    Created the Bottom Navigation Bar  built having IonIcons from expo

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import { useFonts } from 'expo-font';
+import { useCallback } from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import BottomTabNavigation from './navigation/BottomTabNavigation';
+import { NavigationContainer } from '@react-navigation/native';
 
+ const Stack = createNativeStackNavigator();
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+  
+    regular: require('./assets/fonts/Poppins-Regular.ttf'),
+    medium: require('./assets/fonts/Poppins-Medium.ttf'),
+    extraBold: require('./assets/fonts/Poppins-ExtraBold.ttf'),
+    light: require('./assets/fonts/Poppins-Light.ttf'),
+    semiBold: require('./assets/fonts/Poppins-SemiBold.ttf'),
+    bold: require('./assets/fonts/Poppins-Bold.ttf'),
+    
+  })
+
+  // Works like useEffect but also accepts async functions and can return a value .
+ const onLayoutRootView = useCallback(async () => {
+  if (isFontsLoaded) {
+    await SplashScreen.hideAsync();
+  }
+  }, [isFontsLoaded]);
+
+  if (!isFontsLoaded) {
+    return null;
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Bottom Navigation" component={BottomTabNavigation} />
+    </Stack.Navigator>
+    </NavigationContainer>
+
+
+
   );
 }
 
@@ -17,4 +51,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle:{
+    fontFamily: 'regular',
+    fontSize: 15,
+  }
 });
